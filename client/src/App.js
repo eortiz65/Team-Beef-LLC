e70f27e1fd950f97074c71a6aba3dd062a564a3c
eortiz65 <eortiz599@gmail.com>
2022-12-11 14:17:40 -0500

    Home imported to start rendering

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,14 @@
 import './App.css'
+import React, { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './components/Home'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  )
 }
 
 export default App
