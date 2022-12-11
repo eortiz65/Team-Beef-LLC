cea51561685bebca09c4c6d5bfd159fcaabe8045
eortiz65 <eortiz599@gmail.com>
2022-12-11 14:46:46 -0500

    Header also rendered

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,14 @@ import './App.css'
 import React, { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './components/Home'
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="App">
-      <nav></nav>
+      <nav>
+        <Header />
+      </nav>
       <main>
         <Home />
       </main>
