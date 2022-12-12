76760f9af49816694e422b8e42c2418599c82598
eortiz65 <eortiz599@gmail.com>
2022-12-12 11:17:33 -0500

    controllers for admin full CRUD completed

--- controllers/AdminController.js
+++ controllers/AdminController.js
@@ -1,1 +1,61 @@
 const { Admin } = require('../models')
+
+const GetAdmins = async (req, res) => {
+  try {
+    const admins = await Admin.findAll()
+    res.send(admins)
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
+const GetAdmin = async (req, res) => {
+  try {
+    const admin = await Admin.findOne({
+      where: {
+        id: req.params.admin_id
+      }
+    })
+    res.send(admin)
+  } catch (error) {
+    throw error
+  }
+}
+
+const CreateAdmin = async (req, res) => {
+  try {
+    const admin = await Admin.create({ ...req.body })
+    res.send(admin)
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
+const UpdateAdmin = async (req, res) => {
+  try {
+    const admin = await Admin.update(
+      { ...req.body },
+      { where: { id: req.params.admin_id }, returning: true }
+    )
+    res.send(admin)
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
+const DeleteAdmin = async (req, res) => {
+  try {
+    await Admin.destroy({ where: { id: req.params.admin_id } })
+    res.send({ msg: 'Admin Deleted', status: 'Ok' })
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
+module.exports = {
+  GetAdmins,
+  GetAdmin,
+  CreateAdmin,
+  UpdateAdmin,
+  DeleteAdmin
+}
