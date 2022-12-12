f346aa6146c64a462e226fff83bbf29f5c284f56
eortiz65 <eortiz599@gmail.com>
2022-12-12 11:45:12 -0500

    added to main route file

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -1,1 +1,6 @@
 //Main Routes File
+const Router = require('express').Router()
+const AdminRouter = require('./AdminRouter')
+const ClientRouter = require('./ClientRouter')
+Router.use('/admin', AdminRouter)
+Router.use('/client', ClientRouter)
