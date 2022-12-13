9c0d1fa87cdb3e29f10d114b6cf60e0fd4eec49e
eortiz65 <eortiz599@gmail.com>
2022-12-13 09:15:40 -0500

    more corrections

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -1,11 +1,13 @@
 //Main Routes File
-const Router = require('express').Router()
+const router = require('express').Router()
 const AdminRouter = require('./AdminRouter')
 const ClientRouter = require('./ClientRouter')
 const ContractRouter = require('./ContractRouter')
 const ClientContractController = require('./ClientContractRouter')
 
-Router.use('/admin', AdminRouter)
-Router.use('/client', ClientRouter)
-Router.use('/contract', ContractRouter)
-Router.use('/client_contract', ClientContractController)
+router.use('/admin', AdminRouter)
+router.use('/client', ClientRouter)
+router.use('/contract', ContractRouter)
+router.use('/client_contract', ClientContractController)
+
+module.exports = router
--- routes/ClientContractRouter.js
+++ routes/ClientContractRouter.js
@@ -2,3 +2,5 @@ const router = require('express').Router()
 const controller = require('../controllers/ClientContractController')
 
 router.get('/:client_id', controller.GetClientWithContracts)
+
+module.exports = router
