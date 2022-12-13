2bd535187737aef48ad3a4012a6ba6c81a012925
eortiz65 <eortiz599@gmail.com>
2022-12-13 09:54:55 -0500

    testing axios

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import Axios from 'axios'
 
-const HEROKU_URL = 'beef'
-const LOCAL_URL = 'http://localhost:3001/beef'
+const HEROKU_URL = 'http://localhost:3000/beef'
+const LOCAL_URL = 'http://localhost:3000/beef'
 export const BASE_URL =
   process.env.NODE_ENV === 'teambeef_development' ? LOCAL_URL : HEROKU_URL
 
