5b686e14d1c07a2e267fbf5922f819b4b474b771
eortiz65 <eortiz599@gmail.com>
2022-12-14 14:53:41 -0500

    fixed BASE_URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -3,7 +3,8 @@ import Axios from 'axios'
 const HEROKU_URL = 'https://team-beef-llc.herokuapp.com/beef'
 const LOCAL_URL = 'http://localhost:3001/beef'
 export const BASE_URL =
-  process.env.NODE_ENV === 'teambeef_development' ? LOCAL_URL : HEROKU_URL
+  process.env.NODE_ENV === 'production' ? HEROKU_URL : LOCAL_URL
+console.log(BASE_URL)
 
 const Client = Axios.create({ baseURL: BASE_URL })
 
