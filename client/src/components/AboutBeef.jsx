5d8257e963efdd072ad5f7bcd1c68e21147cbb7d
eortiz65 <eortiz599@gmail.com>
2022-12-14 16:17:15 -0500

    rendering media on components

--- client/src/components/AboutBeef.jsx
+++ client/src/components/AboutBeef.jsx
@@ -5,7 +5,14 @@ const AboutBeef = () => {
     <div>
       <section className="about-section">
         <div className="bret">
-          <img src={require("../media/AboutBeef/Bret-Pic.png")} alt="Bret-Pic"/>
+          <img src={require("../media/bret-paladin.png")} alt="Bret-Pic"/>
+          <h1 className="bret-title">Bret "the Righteous Paladin" Borden</h1>
+          <p className="bret-story"><span className="bret-initial">B</span>ret was definitely born to be the face of the party</p>
+        </div>
+        <div className="edward">
+          <img src={require("../media/edward-rogue.png")} alt="Edward-Pic"/>
+          <h1 className="edward-title">Edward "the trickster rogue" Yuabov</h1>
+          <p className="edward-story"><span className="edward-initial">E</span>dward is as cunning as fox.</p>
         </div>
       </section>
     </div>
