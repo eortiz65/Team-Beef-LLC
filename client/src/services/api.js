2f1458e3e2daaa5f4c81b81cec8136b64094d9c9
eortiz65 <eortiz599@gmail.com>
2022-12-14 10:44:33 -0500

    adding heroku link to terniary operator for root

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import Axios from 'axios'
 
-const HEROKU_URL = 'http://localhost:3001/beef'
+const HEROKU_URL = 'https://git.heroku.com/team-beef-llc.git'
 const LOCAL_URL = 'http://localhost:3001/beef'
 export const BASE_URL =
   process.env.NODE_ENV === 'teambeef_development' ? LOCAL_URL : HEROKU_URL
