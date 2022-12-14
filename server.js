b71730cf638d7e988da2bfa40fc9c84b59975ff1
eortiz65 <eortiz599@gmail.com>
2022-12-14 08:32:48 -0500

    morning push

--- server.js
+++ server.js
@@ -9,8 +9,13 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static(`${__dirname}/client/build`))
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => res.json({ message: 'Server Works' }))
 app.use('/beef', AppRouter)
+app.get('/*', (req, res) => {
+  res.sendFile(`${__dirname}/client/build/index.html`)
+})
+
 app.listen(PORT, () => console.log(`Server Started On Port: ${PORT}`))
