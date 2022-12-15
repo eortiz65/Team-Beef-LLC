3b9eefaf4ceeb48d0ea722c2c4341c90f3a0c117
eortiz65 <eortiz599@gmail.com>
2022-12-15 08:17:42 -0500

    trying heroku one more time

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import Axios from 'axios'
 
-const HEROKU_URL = 'https://team-beef-llc.herokuapp.com/'
+const HEROKU_URL = 'https://team-beef-llc.herokuapp.com/beef'
 const LOCAL_URL = 'http://localhost:3001/beef'
 export const BASE_URL =
   process.env.NODE_ENV === 'production' ? HEROKU_URL : LOCAL_URL
